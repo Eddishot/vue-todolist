b5f0eebc1bb4be07f0cc005c4aac48213634bb4c
Eddishot <Eddi_sl94@hotmail.it>
2021-11-02 21:08:59 +0100

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -6,5 +6,31 @@ new Vue({
     el : "#radice",
     data :{
         title: "Vue-Todolist",
+
+        listaDaFare: "",
+        lista:[],
+
+
+    },
+
+    methods:{
+        onClickAdd(){
+
+            // this.lista.push(this.listaDaFare)
+            
+            // this.listaDaFare = ""
+
+            if (this.listaDaFare.trim( )=== ""){
+                return
+            }else{
+                this.lista.push(this.listaDaFare)
+            }
+
+        },
+
+        deleteX(deleteBtn){
+            this.lista.splice(deleteBtn, 1)
+        }
+
     }
 })
